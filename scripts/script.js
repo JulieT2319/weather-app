17f491e3ffb41a87cc0bdf94e8963fcd1c6c5382
Julie T <47932789+JulieT2319@users.noreply.github.com>
2020-01-02 22:00:20 -0700

    current weather pull sketch up

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+var weatherApiKey = "44b3433340eb70a32f38740161f053dc"; //
+
+var citySearch = "Lehi";
+var currentWeatherQuery =
+  "https://api.openweathermap.org/data/2.5/weather?q=" +
+  citySearch +
+  "&APPID=" +
+  weatherApiKey;
+
+$.ajax({
+  url: currentWeatherQuery,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+});
