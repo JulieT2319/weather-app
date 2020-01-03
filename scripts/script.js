94db71a5435da6a5c23f8a7a86e5f27cd0e51a2f
Julie T <47932789+JulieT2319@users.noreply.github.com>
2020-01-03 11:05:27 -0700

    added formmating/necessary styles for site outline

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,17 @@
 var weatherApiKey = "44b3433340eb70a32f38740161f053dc"; //
 
-var citySearch = "Lehi";
-var currentWeatherQuery =
-  "https://api.openweathermap.org/data/2.5/weather?q=" +
-  citySearch +
-  "&APPID=" +
-  weatherApiKey;
+function currentWeather() {
+  var citySearch = "Lehi";
+  var currentWeatherQuery =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    citySearch +
+    "&APPID=" +
+    weatherApiKey;
 
-$.ajax({
-  url: currentWeatherQuery,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-});
+  $.ajax({
+    url: currentWeatherQuery,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
+}
