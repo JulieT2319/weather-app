12a07d0c00eccc0fe028b0b57daebdec9aa0d40d
Julie Theriault <joules2319@gmail.com>
2020-01-03 15:30:05 -0700

    added button add function

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,33 @@
 var weatherApiKey = "44b3433340eb70a32f38740161f053dc"; //
+var citySearch = "";
+var cityArray = [];
 
 function currentWeather() {
-  var citySearch = "Lehi";
-  var currentWeatherQuery =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    citySearch +
-    "&APPID=" +
-    weatherApiKey;
+	var currentWeatherQuery =
+		"https://api.openweathermap.org/data/2.5/weather?q=" +
+		citySearch +
+		"&APPID=" +
+		weatherApiKey;
 
-  $.ajax({
-    url: currentWeatherQuery,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-  });
+	$.ajax({
+		url: currentWeatherQuery,
+		method: "GET"
+	}).then(function (response) {
+		console.log(response);
+	});
 }
+
+function addCityButtons() {
+	$("#cities-searched").empty();
+
+	for (i = 0; i < cityArray.length; i++) {
+		var city = $("<button>");
+		city.addClass("btn btn-outline-info");
+		city.attr("data-city", cityArray[i]);
+		city.text(cityArray[i]);
+		$("#cities-searched").append(city);
+
+	}
+
+}
+
